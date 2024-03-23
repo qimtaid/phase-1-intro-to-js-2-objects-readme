2c393c725acd0bb977087f6d80988538f251e69f
Dan Koskei <qimtaid@gmail.com>
2024-03-23 05:13:20 +0300

    add

--- index.js
+++ index.js
@@ -55,3 +55,17 @@ address.state;
 
 address.zipCode;
 //=> "10004"
+
+
+const wednesdayMenu = {
+    cheesePlate: {
+      soft: "Brie",
+      semiSoft: "Fontina",
+      hard: "Provolone",
+    },
+    fries: "Sweet potato",
+    salad: "Southwestern",
+  };
+  
+  Object.keys(wednesdayMenu);
+  //=> ["cheesePlate", "fries", "salad"]
